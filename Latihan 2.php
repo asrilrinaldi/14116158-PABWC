c54351a683496903983d90e84a7c31a7d7319f90
Asril Rinaldi <asrilrinaldi@gmail.com>
2019-10-23 09:06:58 +0700

    menghitung harga bet

--- Latihan 2.php
+++ Latihan 2.php
@@ -1,42 +1,39 @@
 <?php
- function bar($color="black"){
-    echo 'Nama : <font color="'.$color.'">'.$nama.'</font><br>';
-
- }
-
- $nama = 'Asril rinaldi rinaldi';
- $jumlah_nama = strlen($nama);
-
- 
-
- if ($jumlah_nama <= 10){
-    echo 'Nama : <font color=blue>'.$nama.'</font><br>';
-    echo "Jumlah Nama : ".$jumlah_nama ."<br>";
-    $harga_total = $jumlah_nama * 300;
-    echo "Harga Total Rp." .$harga_total ."<br>";
+ function bet($color="red", $nama='Asril rinaldi'){
     
-
- }else if($jumlah_nama >10 && $jumlah_nama <=20 ){
-    echo 'Nama : <font color=red>'.$nama.'</font><br>';
-    echo "Jumlah Nama : ".$jumlah_nama ."<br>";
-    $harga_total = $jumlah_nama * 500;
-    echo "Harga Total Rp." .$harga_total ."<br>";
-    
-
- }else if($jumlah_nama >20 ){
-    echo 'Nama : <font color=green>'.$nama.'</font><br>';
-    echo "Jumlah Nama : ".$jumlah_nama ."<br>";
-    $harga_total = $jumlah_nama * 700;
-    echo "Harga Total Rp." .$harga_total ."<br>";
    
+    $jumlah_nama = strlen($nama);
+   //  echo "Jumlah Nama : ".$jumlah_nama ."<br>";
+
+
+    if ($jumlah_nama <= 10){
+      echo 'Nama : <font color="'.$color.'">'.$nama.'</font><br>';
+      echo 'Jumlah : <font color="'.$color.'">'.$jumlah_nama.'</font><br>';
+      $harga_total = $jumlah_nama * 300;
+      echo 'Total harga : <font color="'.$color.'">'.$harga_total.'</font><br>';
+      echo "<br>";
+
+   }else if($jumlah_nama >10 && $jumlah_nama <=20 ){
+      echo 'Nama : <font color="'.$color.'">'.$nama.'</font><br>';
+      echo 'Jumlah : <font color="'.$color.'">'.$jumlah_nama.'</font><br>';
+      $harga_total = $jumlah_nama * 500;
+      echo 'Total harga : <font color="'.$color.'">'.$harga_total.'</font><br>';
+      echo "<br>";
+  
+
+   }else if($jumlah_nama >20 ){
+      echo 'Nama : <font color="'.$color.'">'.$nama.'</font><br>';
+      echo 'Jumlah : <font color="'.$color.'">'.$jumlah_nama.'</font><br>';
+      $harga_total = $jumlah_nama * 700;
+      echo 'Total harga : <font color="'.$color.'">'.$harga_total.'</font><br>';
+      echo "<br>";   
+     
+  
+   }
+    
 
  }
- 
- 
- 
-//  bar();
-//  bar('red');
-//  bar('blue');
-//  bar('green');
+
+bet(); 
 
 ?>
